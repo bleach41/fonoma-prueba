6d9d26285f75264f642bfb2732ddf53a95be872e
Tony Alejandro <ftonyalejandrofr@gmail.com>
2023-10-21 19:45:11 -0400

    edit del about

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,7 @@
 export default function About() {
     return (
         <div className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1>Esta es About</h1>
+            <h1>Esta es una calculadora de conversión de moneda creada en Next.js (v 13.5.3) y la API de subscripción gratuita (https://api.exchangerate-api.com)</h1>
         </div>
     )
 }
