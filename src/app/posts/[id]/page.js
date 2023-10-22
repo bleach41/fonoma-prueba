4e1cede0d1e009ecec3ecc4d21c732eacf50b5ce
Tony Alejandro <ftonyalejandrofr@gmail.com>
2023-10-21 20:00:05 -0400

    edit post

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -1,7 +1,9 @@
 export default function Post({ params }) {
     const { id } = params
     return (
+
         <div className="flex min-h-screen flex-col items-center justify-between p-24">
+
             <h1>post {id}</h1>
         </div>
     )
